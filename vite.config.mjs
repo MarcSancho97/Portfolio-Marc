3b7f8a93616eb5997758135fa2b5ce43e4490229
Marc <marcsancho97@gmail.com>
2026-08-31 12:23:57 +0200

    Añadir base path para GitHub Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs.plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': '/src', // Alias para que @ apunte a la carpeta src
-    },
-  },
-  server: {
-    host: 'localhost', // Necesario para el header Referer
-  },
+  base: '/Portfolio-Marc/', // Nombre de tu repositorio rodeado de barras
 })
