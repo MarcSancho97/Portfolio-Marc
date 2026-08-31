95d29e58a64331a9bd61b049cb3db11ffe06c16d
Marc Sancho Garcia <marcsancho97@gmail.com>
2026-08-31 12:44:41 +0200

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue' 
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Portfolio-Marc/', // Nombre de tu repositorio rodeado de barras
+  base: '/Portfolio-Marc/'
 })
